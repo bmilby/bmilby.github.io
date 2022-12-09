123c1f496b01d9a208226a3c4e3b7d11842867a7
Brian Milby <bjones1@V1WRH279F4.grubhub.local>
2022-12-09 17:50:26 -0500

    init

---
+++ events.js
@@ -0,0 +1,10 @@
+
+
+document.addEventListener(`click`, e => {
+    const origin = e.target.closest(`a`);
+
+    if (origin) {
+        console.clear();
+        console.log(`You clicked ${origin.href}`);
+    }
+});
